d3166c1437622877ec5409411e362267cb2eec51
DineshNO <ldineshkumar85@yahoo.com>
2025-11-30 22:33:08 +0400

    remove error message from success response

--- src/main/java/com/marketdata/api/dto/HistoryResponseDto.java
+++ src/main/java/com/marketdata/api/dto/HistoryResponseDto.java
@@ -1,6 +1,7 @@
 package com.marketdata.api.dto;
 
 import com.marketdata.domain.model.Candle;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * Trading View Lightweight Charts format response.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class HistoryResponseDto {
 
     @JsonProperty("s")
